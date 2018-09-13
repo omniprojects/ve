988fc46f8c6e5ee43d7a7851d840451ab541e6be
Matt Billenstein <matt@vazor.com>
2018-09-13 16:55:00 -0700

    Add config_local.sh

---
+++ config_local.sh
@@ -0,0 +1,13 @@
+# install prefix
+VENV="/ove"
+
+# cache package downloads
+PKG_CACHE="/tmp/venv-pkg"
+
+# build directory
+BUILD_DIR="$VENV/build"
+
+# remote rsync for push/pull
+RSYNC_USER="ve"
+RSYNC_HOST="prod-netops.omn1.io"
+RSYNC_PATH="ve"

b7700707380a78d0dc40a4c4d509a64da723a6b5
Matt Billenstein <matt@vazor.com>
2018-11-12 17:34:37 -0800

    Break pkgcache on metabase download

--- pkgs/available/metabase.sh
+++ pkgs/available/metabase.sh
@@ -1,8 +1,8 @@
-METABASE_VERSION="0.30.4"
-
-getpkg http://downloads.metabase.com/v${METABASE_VERSION}/metabase.jar
+METABASE_VERSION="0.31.0"
 
 rm -fR $VENV/opt/metabase
 mkdir -p $VENV/opt/metabase
 
-mv metabase.jar $VENV/opt/metabase/
+getpkg http://downloads.metabase.com/v${METABASE_VERSION}/metabase.jar $BUILD_DIR/metabase-${METABASE_VERSION}.jar
+
+mv $BUILD_DIR/metabase-${METABASE_VERSION}.jar $VENV/opt/metabase/metabase.jar
--- pkgs/metabase.sh
+++ pkgs/metabase.sh
@@ -1,8 +1,1 @@
-METABASE_VERSION="0.31.0"
-
-getpkg http://downloads.metabase.com/v${METABASE_VERSION}/metabase.jar
-
-rm -fR $VENV/opt/metabase
-mkdir -p $VENV/opt/metabase
-
-mv metabase.jar $VENV/opt/metabase/
+available/metabase.sh

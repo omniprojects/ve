598b14ac269a670a5b5854ff44656ad2e4fc0914
Matt Billenstein <matt@vazor.com>
2019-06-17 16:52:53 -0700

    Patch broken cwd / runas behavior in saltstack cmd.run

--- pkgs/zz-site.sh
+++ pkgs/zz-site.sh
@@ -17,3 +17,19 @@ mkdir -p $VENV/share/postgresql/tsearch_data
 cp $SCRIPTPATH/data/postgresql/thesaurus_direct.ths $VENV/share/postgresql/tsearch_data/
 cp $SCRIPTPATH/data/postgresql/en_us.dict $VENV/share/postgresql/tsearch_data/en_us.dict
 cp $SCRIPTPATH/data/postgresql/en_us.affix $VENV/share/postgresql/tsearch_data/en_us.affix
+
+# hax for broken salt - runas / cwd not working on osx
+cd $VENV/lib/python3.6/site-packages/salt/modules
+patch -p0 <<EOF
+--- /Users/mattb/src/omni/om/cmdmod.py	2019-06-17 16:36:50.000000000 -0700
++++ cmdmod.py	2019-06-17 16:36:53.000000000 -0700
+@@ -413,7 +413,7 @@
+         if isinstance(cmd, (list, tuple)):
+             cmd = ' '.join(map(_cmd_quote, cmd))
+ 
+-        cmd = 'su -l {0} -c "{1}"'.format(runas, cmd)
++        cmd = 'su -l {0} -c "cd {1}; {2}"'.format(runas, cwd, cmd)
+         # set runas to None, because if you try to run \`su -l\` as well as
+         # simulate the environment macOS will prompt for the password of the
+         # user and will cause salt to hang.
+EOF

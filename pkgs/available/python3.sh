77be1e6c41b320990004cf5c1743adb9f1cbbfa5
Matt Billenstein <matt@vazor.com>
2019-09-03 16:10:35 -0700

    Update to Python 3.7.4

--- pkgs/available/python3.sh
+++ pkgs/available/python3.sh
@@ -1,6 +1,13 @@
-PYTHON_VERSION="3.6.6"
+PYTHON_VERSION="3.7.4"
+PYTHON_MD5SUM="68111671e5b2db4aef7b9ab01bf0f9be"
 
-getpkg http://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz
+getpkg https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz
+
+if [ "$(md5sum Python-${PYTHON_VERSION}.tgz | awk '{print $1}')" != "$PYTHON_MD5SUM" ]; then
+    echo "Bad release?"
+    exit 1
+fi
+
 tar zxf Python-${PYTHON_VERSION}.tgz
 cd Python-${PYTHON_VERSION}
 ./configure --prefix=$VENV --enable-shared --with-system-expat # --enable-optimizations

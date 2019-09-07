9f4c5377fbc95b5a76c0dd800d8520890e006f07
Matt Billenstein <matt@vazor.com>
2019-09-07 00:03:20 -0700

    Remove a bunch of stuff we don't need any longer - golang et al

--- pkgs/tinyproxy.sh
+++
@@ -1,1 +0,0 @@
-available/tinyproxy.sh
--- pkgs/zz-site.sh
+++ pkgs/zz-site.sh
@@ -1,17 +1,5 @@
 # This is the last package built -- add your dependencies here
 
-export GOROOT="$VENV/opt/go"
-export GOPATH="$BUILD_DIR/go"
-
-rm -fR $GOPATH
-mkdir -p $GOPATH
-
-JUNIT_REPORT_LOCATION=github.com/jstemmer/go-junit-report
-$GOROOT/bin/go get -u $JUNIT_REPORT_LOCATION
-cd "${GOPATH}/src/$JUNIT_REPORT_LOCATION"
-$GOROOT/bin/go build
-mv go-junit-report $VENV/bin
-
 # postgres textsearch thesaurus data
 mkdir -p $VENV/share/postgresql/tsearch_data
 cp $SCRIPTPATH/data/postgresql/thesaurus_direct.ths $VENV/share/postgresql/tsearch_data/
